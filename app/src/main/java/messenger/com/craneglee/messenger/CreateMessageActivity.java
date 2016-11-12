0d88e0dd5ef0f3b53762fc18bcd77e38f3146786
Andrei Kamarou <mr.akomarov@gmail.com>
2016-11-12 10:02:09 +0300

    First commit

---
+++ app/src/main/java/messenger/com/craneglee/messenger/CreateMessageActivity.java
@@ -0,0 +1,25 @@
+package messenger.com.craneglee.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //Вызвать onSendMessage() прищелчке на кнопке
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
